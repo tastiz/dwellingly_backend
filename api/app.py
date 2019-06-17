fea9e1b335e977dfe157aba34c25605b673e6fa6
htharker42 <htharker@gmail.com>
2019-06-17 00:05:46 -0700

    added userclass, setup JWT for API based token managament and authentication

--- api/app.py
+++ api/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
-import os
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
 
 app = Flask(__name__)
+app.secret_key = "1asdfha8dsfh3HUIE"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity ) # /auth
+
 
 @app.route('/')
 def running():
@@ -26,6 +33,7 @@ userList = [
         "password": "userPassword",
         "username": "defaultUser",
         "email": "user1@dwellingly.com",
+        "archived": "false",
         "uid": "user0"
     },
     {
@@ -33,53 +41,74 @@ userList = [
         "password": "userPassword",
         "username": "defaultUser2",
         "email": "user2@dwellingly.com",
+        "archived": "false",
         "uid": "user1"
     }
 ]
 
-# | GET    | `v1/users/`     | Gets all users  
-@app.route('/v1/users/')
-def get_users():
-    return jsonify({"users": userList})
+# | POST   | `v1/users/`     | Creates a new user     |
+# | GET    | `v1/users/`     | Gets all users         |
+class Users(Resource):
+    @jwt_required()
+    def get(self):
+        return {"users": userList}
 
-# | POST   | `v1/users/`     | Creates a new user  
-@app.route('/v1/users', methods=["POST"])
-def create_user():
-    uid = "user" + str(len(userList))
+    def post(self):
+         uid = "user" + str(len(userList)) #replace weith better id hash
+         request_data = request.get_json()
 
-    request_data = request.get_json()
-    new_user = {
-        "name": request_data["name"],
-        "password": request_data["password"],
-        "username": request_data["username"],
-        "email": request_data["email"],
-        "uid":uid
-    }
-    userList.append(new_user)
-    return jsonify(new_user)
-
-# | GET    | `v1/users/:uid` | Gets a single user   
-@app.route('/v1/users/<string:uid>')
-def get_user(uid):
-    for user in userList:
-        if user["uid"] == uid:
-            return jsonify(user)
+         new_user = {
+            "name": request_data["name"],
+            "password": request_data["password"],
+            "username": request_data["username"],
+            "email": request_data["email"],
+            "archived": "false",
+            "uid":uid
+            }
+         userList.append(new_user)
         
-    return jsonify({"error": 301, "message": "User not found"})
+         return new_user, 201
 
+# | GET    | `v1/users/:uid` | Gets a single user     |
+# | PATCH  | `v1/users/:uid` | Updates a single user  |
+# | PUT    | `v1/users/:uid` | Archives a single user |
+# | DELETE | `v1/users/:uid` | Deletes a single user  |
+class User(Resource):
+    @jwt_required()
+    def get(self, uid):
+        user = next(filter(lambda x: x["uid"] == uid, userList), None) 
+        return {"user": user}, 200 if user else 404
 
-# | PATCH  | `v1/users/:uid` | Updates a single user 
-@app.route('/v1/users/<string:username>', methods=["PATCH"])
-def update_user(uid):
-    pass
+    def patch(self, uid):
+        user = next(filter(lambda x: x["uid"] == uid, userList), None) 
+        request_data = request.get_json()
+        new_user = {
+            "name": request_data["name"],
+            "password": request_data["password"],
+            "username": request_data["username"],
+            "email": request_data["email"],
+            "archived": "false",
+            "uid":uid
+            }
+        return {"user": user}, 200 if user else 404
+
+    def put(self, uid):
+        for user in userList:
+            if user["uid"] == uid:
+                if user["archived"] == "false":
+                    user["archived"] = "true"
+                else:
+                    user["archived"] = "false"
+                return user, 201
+    
+        return {"message": "User not found"}, 404
+
+    def delete(self, uid):
+        pass
     
 
-# | DELETE | `v1/users/:uid` | Deletes a single user  |
-@app.route('/v1/users/<string:uid>', methods=["DELETE"])
-def delete_user(uid):
-     pass
-
-
+api.add_resource(Users, "/v1/users")
+api.add_resource(User, "/v1/user/<string:uid>")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
---
+++ api/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, uid, name, password, username, email, archived):
+        self.uid = uid
+        self.name = name
+        self.username = username
+        self.password = password
+        self.email = email
+        self.archived = archived
+        
