b97fad50f77f01a2b4b41e3ce7807bfb88c494e7
Trace Harris <traceh@whiletrace.com>
2019-10-10 00:24:20 -0700

    clean dir

--- api/resources/properties.py
+++ api/resources/properties.py
@@ -15,7 +15,7 @@ from flask_restful import Api, Resource
 
 # single property resource
 class Property(Resource):
-    def get(self,uid):
+    def get(self, uid):
         pass
 
     def patch(self,uid):
@@ -30,10 +30,10 @@ class Property(Resource):
 
 # multiple property resources
 class Properties(Resource):
-    def get(self,uid):
+    def get(self, uid):
         pass
 
-    def post(self,uid):
+    def post(self, uid):
         pass
 
 
